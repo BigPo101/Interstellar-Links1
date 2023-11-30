60999ce60f07bf0815019eeb4b516062ad736526
BigPo101 <123412376+BigPo101@users.noreply.github.com>
2023-11-30 09:54:11 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
-function validation(){
-  if(document.Formfill.Username.value==""){
-    document.getElementById('result').innerHTML="Enter Username*"
-  }
+function signup(e){
+  event.preventDefault():
+
+
+var email = document.getElementById('email').value;
+  var username = document.getElementById('username').value;
+  var pass = document.getElementById('password').value;
+
+var user = {
+  email: email,
+  username: username;
+  password: pass;
+};
+
+var json = JSON.stringify(user);
+localStorage.setItem(username, json);
+console.log('user added');
+
+}
+
+
+function login(e){
+event.preventDefault();
+  console.log(123);
 }
